57c353d562f66f4990b88019b10ead77bcd85b0b
Allan Zheng <azheng34@outlook.com>
2020-08-29 11:16:30 -0400

    react version of tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Board from "./Components/Board";
+import Game from "./Components/Game";
 
 function App() {
-  return <Board />;
+  return <Game />;
 }
 
 export default App;
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export default class Square extends React.Component {
-  render() {
-    return <button className="square">{this.props.value}</button>;
-  }
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
